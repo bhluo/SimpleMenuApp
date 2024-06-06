e886e99ded2ece0610649d566f024e00ca1ecb02
bhluo <135720447+bhluo@users.noreply.github.com>
2024-06-06 18:21:59 +0800

    first commit

---
+++ wpfOrder/Models/Dish.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfOrder.Models
+{
+    public class Dish
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Comment { get; set; }
+        public double Score { get; set; }
+        public string Price { get; set; }
+    }
+}
---
+++ wpfOrder/Services/IDataService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wpfOrder.Models;
+
+namespace wpfOrder.Services
+{
+    public interface IDataService
+    {
+        List<Dish> GetDishes();
+    }
+}
