7dffc85c74251495f208c2e45cea8b73fe330847
bhluo <135720447+bhluo@users.noreply.github.com>
2024-06-06 18:19:20 +0800

    first commit

---
+++ wpfOrder/Models/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfOrder.Models
+{
+    public class Restaurant
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ wpfOrder/Services/MockOrderService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfOrder.Services
+{
+    public class MockOrderService : IOrderService
+    {
+        public void PlaceOrder(List<string> dishes)
+        {
+            System.IO.File.WriteAllLines(@"D:\order.txt", dishes.ToArray());
+        }
+    }
+}
